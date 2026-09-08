78c9b3e46ae402fa1766b6b88a266178b47ac097
Heidi <heidi.hussein99@eng-st.cu.edu.eg>
2026-09-09 00:57:39 +0300

    Added screenshot-on-failure

---
+++ automation/saucedemo/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
--- automation/saucedemo/test_saucedemo_e2e.py
+++ automation/saucedemo/test_saucedemo_e2e.py
@@ -11,19 +11,29 @@ import os
 
 # Setup the driver
 @pytest.fixture
-def driver():
+def driver(request):
     service = Service(ChromeDriverManager().install())
     options = Options()
     if os.environ.get("CI"):
         options.add_argument("--headless=new")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
     browser = webdriver.Chrome(service=service, options=options)
     browser.get("https://www.saucedemo.com/")
+
     yield browser
+
+    # If the test failed, save a screenshot before quitting
+    if request.node.rep_call.failed:
+        name = request.node.name.replace("[", "_").replace("]", "").replace(" ", "_")
+        browser.save_screenshot(f"failure_{name}.png")
+
     browser.quit()
 
 
 def test_valid_login(driver):
-    wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 20)
     wait.until(EC.presence_of_element_located((By.ID, "user-name"))).send_keys("standard_user")
     wait.until(EC.presence_of_element_located((By.ID, "password"))).send_keys("secret_sauce")
     wait.until(EC.presence_of_element_located((By.ID, "login-button"))).click()
@@ -34,7 +44,7 @@ def test_valid_login(driver):
 
 
 def test_invalid_login(driver):
-    wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 20)
     wait.until(EC.presence_of_element_located((By.ID, "user-name"))).send_keys("wrong_user")
     wait.until(EC.presence_of_element_located((By.ID, "password"))).send_keys("wrong_pass")
     wait.until(EC.presence_of_element_located((By.ID, "login-button"))).click()
@@ -53,7 +63,7 @@ def test_invalid_login(driver):
     "Sauce Labs Fleece Jacket"
 ])
 def test_add_to_cart_and_checkout(driver, target_item_name):
-    wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 20)
     wait.until(EC.presence_of_element_located((By.ID, "user-name"))).send_keys("standard_user")
     wait.until(EC.presence_of_element_located((By.ID, "password"))).send_keys("secret_sauce")
     wait.until(EC.element_to_be_clickable((By.ID, "login-button"))).click()
@@ -72,7 +82,8 @@ def test_add_to_cart_and_checkout(driver, target_item_name):
             break
 
     add_to_cart_button = target_item.find_element(By.CSS_SELECTOR, '[data-test^="add-to-cart"]')
-    wait.until(EC.element_to_be_clickable(add_to_cart_button)).click()
+    driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", add_to_cart_button)
+    add_to_cart_button.click()
 
     wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "shopping_cart_link"))).click()
     wait.until(EC.url_to_be("https://www.saucedemo.com/cart.html"))
